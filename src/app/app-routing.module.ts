4b4a65d1c8811c47f0be4c749ff48d5d39f0a5a6
Daniel Mateus Ovalle <emateus@bancolombia.com.co>
2022-02-16 21:49:40 -0500

    add view login

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './views/login/login.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'login', component: LoginComponent
+  },
+  {
+    path: '**', pathMatch: 'full', redirectTo: 'login'
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
